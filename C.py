94fbfebbfe939663300922832b742a09c44d3a9b
Enrique Marina <eherraiz@myuax.com>
2022-03-20 19:26:27 +0100

    gdrhg

--- C.py
+++ C.py
@@ -18,7 +18,7 @@ print(a().y((a,)))
 print(A.y(aa, (a,z)))
 print(aa.y((z,1,'z')))
 
-print('puerca')
+rgngrnl
 #el primer print da la clase A
 #el segundo print devolvera true porque se cumple
 #el tercer print devuelve 0 porque le pasas una tupla
