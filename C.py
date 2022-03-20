1f28cfef44785fa1653185320aebd64e2d349200
Enrique Marina <eherraiz@myuax.com>
2022-03-20 19:14:22 +0100

    df

--- C.py
+++ C.py
@@ -17,3 +17,11 @@ print(z(()))
 print(a().y((a,)))
 print(A.y(aa, (a,z)))
 print(aa.y((z,1,'z')))
+
+print('puerca')
+#el primer print da la clase A
+#el segundo print devolvera true porque se cumple
+#el tercer print devuelve 0 porque le pasas una tupla
+#el cuarto devuelve 1 porque llama a la funcion y de la clase a y le pasa la tupla con un elemento
+#el quinto devuelve 2 porque llama a la funcion y de A donde en el self le pasa aa que es la clase a y luego una tupla de dos elementos
+#el sexto devuelve 3 porque vuelve a hacer lo anterior pasando 3 elementos
