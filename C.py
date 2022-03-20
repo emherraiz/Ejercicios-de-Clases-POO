1106df8265e3fab980b46eab4629d5566b68d057
Enrique Marina <eherraiz@myuax.com>
2022-03-20 19:42:07 +0100

    gf

--- C.py
+++ C.py
@@ -18,7 +18,6 @@ print(a().y((a,)))
 print(A.y(aa, (a,z)))
 print(aa.y((z,1,'z')))
 
-rgngrnl
 #el primer print da la clase A
 #el segundo print devolvera true porque se cumple
 #el tercer print devuelve 0 porque le pasas una tupla
