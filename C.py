6594aaad45ec70b0f596ca7d92cabf1e24876b20
Enrique Marina <eherraiz@myuax.com>
2022-03-25 11:34:09 +0100

    hola

--- C.py
+++ C.py
@@ -1,5 +1,3 @@
-# Enunciado: adivina qué mensajes se muestran mediante el siguiente código voluntariamente poco comprensible:
-
 class A:
     def z(self):
         return self
@@ -26,11 +24,3 @@ print(f'y = {y}')
 print(f'aa = {aa}')
 print(f'z = {z}')
 
-
-
-#el primer print da la clase A
-# el segundo print devolvera false porque se cumple
-#el tercer print devuelve 0 porque le pasas una tupla vacia a la funcion y de la clase a
-#el cuarto devuelve 1 porque llama a la funcion y de la clase a y le pasa la tupla con un elemento
-#el quinto devuelve 2 porque llama a la funcion y de A donde en el self le pasa aa que es la clase a y luego una tupla de dos elementos
-#el sexto devuelve 3 porque vuelve a hacer lo anterior pasando 3 elementos
